d12bcde44ca267adb8bb9a05ea3e73cab7aaa705
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-11 08:27:43 +0330

    feat: handle state categories

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import { getAllCategories, getMainCategories } from "./utils/utlis";
 function App() {
   const [menus, setMenus] = useState(menu);
   console.log(menus);
-  
+  const [categories, setCategories] = useState(getMainCategories(menus));
+  console.log(categories);
 
   return <div></div>;
 }
